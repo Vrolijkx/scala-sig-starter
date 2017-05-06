14ea3f5ee6139cd795972ecd8b38b9d627ff4ac2
Kristof Vrolijkx <kristof.vrolijkx@aca-it.be>
2017-05-06 14:44:45 +0200

    moving some code + adding some solutions.

---
+++ src/test/scala/be/aca/scala/session1/exercises/exercise1/CrappyCalculatorSpec.scala
@@ -0,0 +1,10 @@
+package be.aca.scala.session1.exercises.exercise1
+
+import org.scalatest.FunSuite
+
+/**
+  * Copyright (C) 26/04/2017 - REstore NV
+  */
+class CrappyCalculatorSpec extends FunSuite {
+
+}
